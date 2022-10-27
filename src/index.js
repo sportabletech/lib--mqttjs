bcebc8f8aaabf89aebd288a9bdfbc123b47ebac5
Tom Duckworth <tom.duckworth91@gmail.com>
2022-10-27 12:02:18 +0100

    import library and mqtt.js from MT

---
+++ src/index.js
@@ -0,0 +1,84 @@
+import { connect } from 'mqtt'
+import { uInt8ToJSON } from '../../utils/helpers'
+
+export const MqttController = {
+    Client: function (name, locationName) {
+        this.name = name
+        this.locationName = locationName
+
+
+        this.connect = function (url, options, success, error, initialSubscription, initialSubscriptionMessageHandler) {
+            this.client = connect(url, options)
+
+            this.callbacks[initialSubscription] = initialSubscriptionMessageHandler
+
+            this.client.on('connect', (connack) => {
+                // console.log(connack)
+                Object.keys(this.callbacks).forEach((key) => {
+                    this.client.unsubscribe(key)
+                    this.client.subscribe(key)
+                })
+                success()
+            })
+
+            // error
+            this.client.on('close', (e) => {
+                console.log('sporstcaster closed')
+            })
+            this.client.on('offline', (e) => {
+                this.error(error, 'offline')
+            })
+            this.client.on('disconnect', () => this.error(error, 'disconnect'))
+            this.client.on('error', () => this.error(error, 'error'))
+            this.client.on('message', this.handleMessage.bind(this))
+            // this.client.on('packetreceive', (packet) => {
+            //   if (packet.cmd === 'pingresp') {
+            //     console.log(name, ': ', packet)
+            //   }
+
+            // })
+            // this.client.on('packetsent', (packet) => {
+            //   console.log(name, ': ', packet)
+
+            // })
+
+        }
+
+        this.handleMessage = function (topic, message) {
+            if (message.length > 0) {
+                let temp = uInt8ToJSON(message)
+                for (let key in this.callbacks) {
+                    if (key === topic) {
+                        let callback = this.callbacks[key]
+                        callback(temp, topic)
+                    }
+                }
+            }
+        }
+
+        this.callbacks = {}
+
+        this.subscribe = function (topic, callback) {
+            console.log('subscribing: ', topic)
+            this.client.subscribe(topic)
+
+            this.callbacks[topic] = callback
+        }
+
+        this.unsubscribe = function (topic) {
+            // console.log('unsubscribing: ', topic)
+            delete this.callbacks[topic]
+            this.client.unsubscribe(topic)
+        }
+
+        this.endClient = function () {
+            console.log('ending client')
+            this.client.end()
+        }
+
+        this.error = function (error, type) {
+            console.log(type)
+            error()
+        }
+    }
+}
